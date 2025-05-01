ab0b3c5ea1485b780e1cc7a59af859794b176ec6
Tomato <91081648+TomatoSeal@users.noreply.github.com>
2025-05-01 22:15:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["198852782604812289", ""], 
   mongodbUri : "",
   spotifyClientId : "",
   spotifyClientSecret : "",
@@ -12,7 +12,7 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/miqiling",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
